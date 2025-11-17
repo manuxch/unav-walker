ee6840cfab116166a6fe46ff9ca8399013f2665f
manuxch <manux.ch@gmail.com>
2025-11-17 20:23:39 -0300

    ajuste de velocidad de drift y directorio de resultados

---
+++ resultados/resultados-log.md
@@ -0,0 +1,10 @@
+# Velocidades de partícula única
+2025.11.17
+
+Para ver si está bien parametrizado el sistema, ajusté los valores de las unidades reducidas a partir de los valores experimentales (eso está en `unav-walkers/utils/reduced_units.ods`).
+
+Hice una simulación con pocas partículas he hice el seguimiento de su posición y velocidad. Como solo corrí unas 10 partículas, éstas no colisionan entre si.
+
+Al hacer el seguimiento de las que inicialmente están lejos del "base", todas las velocidades de drift dieron iguales. Las figuras `vy-t-fit.pdf`, `vy-t-colision.pdf` y `vy-t-base.pdf` muestran distintos períodos del segimiento de la partícula 1: durante el desplazamiento libre (que ajusta la velocidad de drift), al colisionar con la base, y cuando queda rebotando contra la misma.
+
+El ajuste obtenido de la velocidad de drift es similar al que midieron experimentalmente: 1 cm/s.
--- scripts/anal-vel.py
+++ scripts/anal-vel.py
@@ -4,27 +4,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 from scipy.stats import linregress
-plt.style.use('/home/manuel/granular/unav-walker/utils/figs.mplstyle')
+plt.style.use('../../utils/figs.mplstyle')
+# plt.style.use('/home/manuel/granular/unav-walker/utils/figs.mplstyle')
 
 files = glob.glob('*.ve')
 print(len(files))
 files.sort()
 x_s_2_e = 0.005
 v_s_2_e = 0.221359436211787
-t_s_2_e = 0.000245
-
+t_s_2_e = 0.0225876975726313
 ts = []
 ys = []
 vels = []
-n_start = 100
-n_end = 1500
+# # Valores para obtener velocidad de drift
+# n_start = 100
+# n_end = 500
+# Valores para ver la colisión
+n_start = 7200
+n_end = 7500
+pid = 1
 for f in files[5:]:
     fin = open(f, 'r')
     data = fin.readlines()
     fin.close()
     ts.append(float(data[0].split()[12]) * t_s_2_e)
-    ys.append(float(data[3].split()[3]) * x_s_2_e)
-    vels.append(float(data[3].split()[5]) * v_s_2_e * 100)
+    for fila in data[1:]:
+        fila = fila.split()
+        if int(fila[0]) == pid:
+            ys.append(float(fila[3]) * x_s_2_e)
+            vels.append(float(fila[5]) * v_s_2_e * 100)
 
 ats = np.array(ts)
 ays = np.array(ys)
@@ -37,10 +45,10 @@ print(f'fit vel: {vel:.3f}')
 # plt.plot(ats, avels)
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
 
-ax1.plot(ats[n_start:n_end], ays[n_start:n_end], label=fr'$v_y = {vel:.3f}$ [m/s]')
+ax1.plot(ats[n_start:n_end], ays[n_start:n_end] * 100, label=fr'$v_y = {100 * vel:.3f}$ [cm/s] (fit)')
 ax1.set_ylabel(r'$y$ [cm]')
 ax1.legend()
-ax2.plot(ats[n_start:n_end], avels[n_start:n_end], label=fr'$<v_y> = {vmean:.3f}$ [cm/s]')
+ax2.plot(ats[n_start:n_end], avels[n_start:n_end], label=fr'$\langle v_y \rangle = {vmean:.3f}$ [cm/s]')
 ax2.set_xlabel('$t$ [s]')
 ax2.set_ylabel(r'$v_y$ [cm/s]')
 ax2.legend()
